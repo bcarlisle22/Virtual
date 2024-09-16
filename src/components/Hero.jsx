124aecdaeed81ff84a1a802df8225778ca8dd4a0
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2024-09-16 18:35:04 -0500

    Add files via upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        {" "}
+        VirtualR build tools&nbsp;
+        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+          for developers{" "}
+        </span>
+      </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        {" "}
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive developer tools. Get started today and turn your imagination
+        into immersive reality!{" "}
+      </p>
+
+      <div className="flex justify-center my-10">
+        <a
+          href="#"
+          className=" bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md mx-10"
+        >
+          Start for free
+        </a>
+        <a
+          href="#"
+          className="py-2 px-3 border rounded-md hover:bg-white hover:text-black transition delay-150 duration-300 ease-in-out"
+        >
+          Documentation
+        </a>
+      </div>
+
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border-orange-700 shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video1} type="video/mp4" />
+        </video>
+
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border-orange-500 shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video2} type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
